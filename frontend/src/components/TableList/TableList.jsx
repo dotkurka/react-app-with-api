1e8847563849095c888492ad4c7fae4245fb1a02
dotkurka <moizeromg@gmail.com>
2022-10-14 18:18:51 +0300

    add MUI

---
+++ frontend/src/components/TableList/TableList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PostTable from "../PostTable/PostTable";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material/";
+
+const TableList = ({ posts }) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">id</TableCell>
+                        <TableCell align="right">Name</TableCell>
+                        <TableCell align="right">email</TableCell>
+                        <TableCell align="right">age</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {posts.map((post) => (
+                        <PostTable key={post.id} post={post} />
+                    ))}
+                </TableBody>
+                <tfoot></tfoot>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default TableList;

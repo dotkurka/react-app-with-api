36b52b800ccaf250f093d97d8f674bcc208314cf
dotkurka <moizeromg@gmail.com>
2022-10-17 23:04:15 +0300

    done review

--- frontend/src/api/getUsers.js
+++ frontend/src/api/getUsers.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+const blabla = process.env.REACT_APP_HOST_ADRESS;
+console.log(blabla);
 
 const requesUser = async () => {
-    return await axios.get("http://localhost:3000/users/").then((data) => data.data);
+    return await axios.get(`${process.env.REACT_APP_HOST_ADRESS}/users/`).then((data) => data.data);
 };
 
 export default requesUser;
